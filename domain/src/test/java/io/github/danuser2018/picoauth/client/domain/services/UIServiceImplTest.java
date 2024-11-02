c0aeee36572fa6ff89d2346a57a3e096a0fe4a7e
danuser2018 <danuser2018@outlook.es>
2024-11-02 19:12:30 +0100

    Complete the modularization

--- domain/src/test/java/io/github/danuser2018/picoauth/client/domain/services/UIServiceImplTest.java
+++ domain/src/test/java/io/github/danuser2018/picoauth/client/domain/services/UIServiceImplTest.java
@@ -1,19 +1,13 @@
 package io.github.danuser2018.picoauth.client.domain.services;
 
 import io.github.danuser2018.picoauth.client.domain.ports.outbound.StartUIPort;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 public class UIServiceImplTest {
 
-    private UIServiceImpl uiServiceImpl;
     private final StartUIPort startUIPort = Mockito.mock();
-
-    @BeforeEach
-    void beforeTest() {
-        uiServiceImpl = new UIServiceImpl(startUIPort);
-    }
+    private final UIServiceImpl uiServiceImpl = new UIServiceImpl(startUIPort);
 
     @Test
     void testStartUI() {
