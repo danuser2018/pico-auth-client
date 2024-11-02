46a47a50c12368b9200b747ff0fe98afadf20bf2
danuser2018 <danuser2018@outlook.es>
2024-11-02 09:40:55 +0100

    New button to add a new identity (only UI)

--- infra-swing/src/main/java/io/github/danuser2018/picoauth/client/infra/swing/components/MainJFrame.java
+++ infra-swing/src/main/java/io/github/danuser2018/picoauth/client/infra/swing/components/MainJFrame.java
@@ -1,28 +1,64 @@
 package io.github.danuser2018.picoauth.client.infra.swing.components;
 
 import io.github.danuser2018.picoauth.client.infra.swing.NoHeadlessUIAdapter;
-import lombok.NonNull;
+import jiconfont.icons.font_awesome.FontAwesome;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
+import java.awt.*;
 
+import static java.awt.BorderLayout.CENTER;
+import static java.awt.BorderLayout.SOUTH;
+import static java.awt.Color.lightGray;
+import static java.awt.FlowLayout.RIGHT;
+import static javax.swing.BorderFactory.createEmptyBorder;
+import static jiconfont.swing.IconFontSwing.buildIcon;
+import static jiconfont.swing.IconFontSwing.buildImage;
+
+@Slf4j
 @Component
 @ConditionalOnBean(NoHeadlessUIAdapter.class)
 public class MainJFrame extends JFrame {
 
-    private final static String TITLE = "PicoAuth - Client";
+    private JButton newIdentityButton;
 
     public MainJFrame(@Value("${user-interface.main-frame.width:800}") final int width,
-                      @Value("${user-interface.main-frame.height:600}") final int height,
-                      @NonNull final ImageIcon logoIcon
+                      @Value("${user-interface.main-frame.height:600}") final int height
     ) {
-        super(TITLE);
-
-        setIconImage(logoIcon.getImage());
+        super("PicoAuth - Client");
+        setIconImage(buildImage(FontAwesome.KEY, 16, lightGray));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(width, height);
         setLocationRelativeTo(null);
+
+        createGUI();
+        composeGUI();
+    }
+
+    private void createGUI() {
+        newIdentityButton = new JButton(buildIcon(FontAwesome.PLUS, 32, lightGray));
+        newIdentityButton.setToolTipText("Crea una nueva identidad");
+        newIdentityButton.setBorder(createEmptyBorder(10, 40, 10, 40));
+        newIdentityButton.addActionListener(event -> addNewIdentity());
+    }
+
+    private void composeGUI() {
+        setLayout(new BorderLayout());
+
+        var identitiesPanel = Box.createGlue();
+
+        var actionsPanel = new JPanel(new FlowLayout(RIGHT));
+        actionsPanel.setBorder(createEmptyBorder(0, 20, 20, 20));
+        actionsPanel.add(newIdentityButton);
+
+        add(identitiesPanel, CENTER);
+        add(actionsPanel, SOUTH);
+    }
+
+    private void addNewIdentity() {
+        log.info("Creating a new identity ...");
     }
 }
